1fdec5db294c472930889c50349cd3ce189ecde9
MAX FELDMAN <maxfeldman99@gmail.com>
2020-04-03 21:15:22 +0300

    table added with logos and source control reference

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,21 @@ import {
   Table,
   Avatar,
   Spin,
-  Icon,
   Modal,
   Empty
 } from 'antd';
+import Icon from '@ant-design/icons';
+import {ReactComponent as Java} from './assets/javaLogo.svg'
+import {ReactComponent as CSS} from './assets/cssLogo.svg'
+import {ReactComponent as HTML} from './assets/htmlLogo.svg'
+import {ReactComponent as JavaScript} from './assets/javaScriptLogo.svg'
+import {ReactComponent as Python} from './assets/pythonLogo.svg'
+import {ReactComponent as CPlusPlus} from './assets/cplusplusLogo.svg'
+import {ReactComponent as Github} from './assets/github.svg'
+import {ReactComponent as test} from './assets/test.svg'
+import {ReactComponent as Coding} from './assets/codingLogo.svg'
 
+//import ArchiveSVG from './assets'
 
 class App extends Component {
 
@@ -34,13 +44,56 @@ fetchRepos = () => {
   }));
 }
 
+getComponent = (name) =>{
+  switch (name) {
+
+    case 'Java':
+      return Java
+
+    case 'CSS':
+      return CSS
+
+    case 'JavaScript':
+      return JavaScript
+
+    case 'HTML':
+      return HTML
+
+    case 'Jupyter Notebook':
+      return Python
+
+    case 'Jupyter Notebook':
+      return Python
+
+    case 'Python':
+        return Python
+    case 'C++':
+        return CPlusPlus
+
+    default:
+      return Coding
+  }
+}
+
+
 
   render(){
 
     const { repos } = this.state;
 
-    if (repos && repos.length){
+    var dict = {
+      'Java': "Java",
+      'CSS': "CSS",
+      'HTML': "HTML",
+      'Jupyter Notebook': "Python",
+      'JavaScript': 'JavaScript'
+    
+    };
 
+    
+
+    if (repos && repos.length){
+    
       const columns = [
         {
           title: 'Repository Name',
@@ -53,34 +106,66 @@ fetchRepos = () => {
           key: 'language'
         },
         {
+          title: '',
+          dataIndex: 'avatar',
+          render: (text, language) => (
+             
+            <Icon style={{ fontSize: '44px'}} component={this.getComponent(language.language)}  />
+          )
+        },
+        {
           title: 'Created at',
           dataIndex: 'created_at',
           key: 'created_at'
         },
         {
-          title: 'Updated at',
+          title: 'Last updated',
           dataIndex: 'updated_at',
           key: 'updated_at'
         },
         {
           title: 'Source code',
           dataIndex: 'html_url',
-          render: (text, record) => <a href={record.html_url}>Click here</a>
-        }
-  
+          render: (text, record) => {
+            return (
+              <Icon style={{ fontSize: '44px'}} component={Github} onClick={(e) => { 
+                e.preventDefault();
+                window.location = record.html_url;    
+               }} />
+
+            );
+          }
+        }  
       ];
     
 
     return (
     <Table
+    onRow={(record, rowIndex) => {
+      return {
+        onClick: event => {
+          if (1){
+            console.log('AAA')
+            console.log(rowIndex);
+            console.log(record);
+          }
+        }, // click row
+        onDoubleClick: event => {}, // double click row
+        onContextMenu: event => {}, // right button click row
+        onMouseEnter: event => {}, // mouse enter row
+        onMouseLeave: event => {}, // mouse leave row
+      };
+    }}
       dataSource={repos}
       columns={columns}
       rowKey='name' />
-      );
+       );
+
+      
   }
 
   return <h1>Nothing to show</h1>
-    
+ 
  }
 }
 
